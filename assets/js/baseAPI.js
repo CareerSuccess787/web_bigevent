2e1f04251094f2c02cb3dd86a2757e65b38f2963
wangzhanqi <lovetimecolor@163.com>
2020-12-01 20:51:38 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,35 @@
 // 这个函数中可以拿到我们给ajax提供的配置对象
 // 这个options就是调用ajax时传递回来的配置对象
 $.ajaxPrefilter(function(options) {
+
     // 在发起真正的ajax请求之前， 统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
+
+    // console.log(options.url);
+
+    // 统一为有权限的接口，设置headers请求头
+    // indexOf() 数组中查找给定元素的第一个索引， 如果存在就返回索引号， 不存在就返回 - 1
+    // 如果地址里有/my/就给一个请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            // 要是有值就获取token 没有就给一个空值
+            // localStorage 用于长久保存整个网站的数据，保存的数据没有过期时间，直到手动去删除。localStorage 属性是只读的。
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete 回调函数
+    // 无论成功还是失败最终都会调用这个complete回调函数
+    options.complete = function(res) {
+        // console.log('执行了 complete回调函数：');
+        // console.log(res);
+        // 在这个回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据 (response响应)
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空'token'
+            localStorage.removeItem('token')
+
+            // 2.强制跳转到登录页
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,66 @@
 $(function() {
-  // 调用 getUserInfo 获取用户基本信息
-  getUserInfo()
+    // 调取“获取用户基本信息”的函数
+    getUserInfo()
+    var layer = layui.layer
+    $('#btnLogout').on('click', function() {
+        //    提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token')
+                // 2.重新跳转到登录页面
+            location.href = '/login.html'
+            console.log(11);
 
-  var layer = layui.layer
 
-  // 点击按钮，实现退出功能
-  $('#btnLogout').on('click', function() {
-    // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
-      //do something
-      // 1. 清空本地存储中的 token
-      localStorage.removeItem('token')
-      // 2. 重新跳转到登录页面
-      location.href = '/login.html'
-
-      // 关闭 confirm 询问框
-      layer.close(index)
+            // 关闭confirm询问框（官方自带）
+            layer.close(index);
+        });
     })
-  })
 })
 
 // 获取用户的基本信息
 function getUserInfo() {
-  $.ajax({
-    method: 'GET',
-    url: '/my/userinfo',
-    success: function(res) {
-      if (res.status !== 0) {
-        return layui.layer.msg('获取用户信息失败！')
-      }
-      // 调用 renderAvatar 渲染用户的头像
-      renderAvatar(res.data)
-    }
-    // 不论成功还是失败，最终都会调用 complete 回调函数
-    // complete: function(res) {
-    //   // console.log('执行了 complete 回调：')
-    //   // console.log(res)
-    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-    //     // 1. 强制清空 token
-    //     localStorage.removeItem('token')
-    //     // 2. 强制跳转到登录页面
-    //     location.href = '/login.html'
-    //   }
-    // }
-  })
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers就是请求头配置对象
+        // 要是有值就获取token 没有就给一个空值
+        // localStorage 用于长久保存整个网站的数据，保存的数据没有过期时间，直到手动去删除。localStorage 属性是只读的。
+        // 这行代码已经在baseAPI里了
+        // headers: {
+        // Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message)
+            }
+            console.log(res.message);
+            // 调用'渲染用户的头像'的函数
+            renderAvatar(res.data)
+        }
+    })
 }
 
-// 渲染用户的头像
+// 渲染用户头像：render[ˈran dər] 渲染 Avatar[ˈa və tao] 化身， 头像， 阿凡达
 function renderAvatar(user) {
-  // 1. 获取用户的名称
-  var name = user.nickname || user.username
-  // 2. 设置欢迎的文本
-  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-  // 3. 按需渲染用户的头像
-  if (user.user_pic !== null) {
-    // 3.1 渲染图片头像
-    $('.layui-nav-img')
-      .attr('src', user.user_pic)
-      .show()
-    $('.text-avatar').hide()
-  } else {
-    // 3.2 渲染文本头像
-    $('.layui-nav-img').hide()
-    var first = name[0].toUpperCase()
-    $('.text-avatar')
-      .html(first)
-      .show()
-  }
+
+    // 1.获取用户的名称   
+    var name = user.nickname || user.username // 这里用了'逻辑或': 1 真返1, 1 假返2。有一个真的就返回真的。有昵称就返回昵称，没有昵称就返回用户名
+
+    // 2.设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+
+    // 3. 按需渲染用户的头像（ 有头像就渲染头像， 没有头像渲染文本头像）
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.tex-avatar').hide()
+    } else {
+        // 3.2 渲染文本头像
+        $('.layui-nav-img').hide()
+
+        // 获取字符串中的第一个字符可以用'name[0]'这样。当数组用。.toUpperCase()将字母转成大写
+        var first = name[0].toUpperCase();
+        $('.tex-avatar').html(first).show()
+    }
 }
